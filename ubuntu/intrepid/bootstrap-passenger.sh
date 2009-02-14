67edf6e755e795772307fa38552b63bcbb405171
Michael Lang <mwlang@cybrains.net>
2009-02-14 06:35:54 -0500

    bootstrap for passenger

---
+++ ubuntu/intrepid/bootstrap-passenger.sh
@@ -0,0 +1,6 @@
+bootstrap.sh
+mri-ruby-187-package.sh
+rubygems.sh
+rails.sh
+apache2.sh
+passenger.sh
--- ubuntu/intrepid/passenger.sh
+++ ubuntu/intrepid/passenger.sh
@@ -1,6 +1,6 @@
-sh apache2.sh
-
 sudo gem install passenger -v 2.0.6
 sudo passenger-install-apache2-module
 
 sudo cp passenger.load /etc/apache2/mods-available/passenger.load
+sudo a2enmod passenger
+sudo /etc/init.d/apache2 restart
