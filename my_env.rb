2a7d778efd2e535ddf4d5760d6e2756acc2cbfab
Michael Lang <mwlang@cybrains.net>
2009-02-14 15:55:52 -0500

    passenger detect corrected

--- my_env.rb
+++ my_env.rb
@@ -18,7 +18,7 @@ end
 
 puts "OS: #{get_version('uname', '-r -m -o')}"
 puts "Apache: #{get_version('apache2', '-v').first}"
-puts "Passenger: #{get_version('passenger', '--version')}"
+puts "Passenger: #{get_version('passenger-config', '--version')}"
 puts "Ruby: #{get_version('ruby', '--version')}"
 puts "Rubygems: #{get_version('gem', '--version')}"
 puts "gems:\n#{listing(get_version('gem', 'list'))}"
