3d99653bb5a99fff89358a24b40062ce7c4115a2
Michael Lang <mwlang@cybrains.net>
2009-02-14 06:13:56 -0500

    fine-tuning scripts

--- ubuntu/intrepid/rubygems.sh
+++ ubuntu/intrepid/rubygems.sh
@@ -3,7 +3,7 @@ wget http://rubyforge.org/frs/download.php/45905/rubygems-1.3.1.tgz
 
 tar xvzf rubygems-1.3.1.tgz
 cd rubygems-1.3.1
-sudo ruby setup.rb
+sudo ruby setup.rb --no-ri --no-rdoc
 sudo ln -s /usr/bin/gem1.8 /usr/bin/gem
 
 cd ..
